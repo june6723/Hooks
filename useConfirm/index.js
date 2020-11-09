b5725d6024df4b050eaee9be58b0ef8d49075a72
June Kim <june6723@naver.com>
2020-11-10 00:37:06 +0900

    usePreventLeave

---
+++ useConfirm/index.js
@@ -0,0 +1,1 @@
+import { useConfirm } from "./useConfirm";
---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,16 @@
+export const useConfirm = (message = "", onComfirm, onCancel) => {
+  if (!onComfirm || typeof onComfirm !== "function") {
+    return;
+  }
+  if (onCancel && typeof onCancel !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      onComfirm();
+    } else {
+      onCancel();
+    }
+  };
+  return confirmAction;
+};
