002bde8513b8347f08cd21171fd999ebff49524b
june6723 <june6723@naver.com>
2020-12-17 22:49:54 +0900

    Hooks challenge complete

--- challenge/src/hooks/useLockScroll/useLockScroll.js
+++ challenge/src/hooks/useLockScroll/useLockScroll.js
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 const useLockScroll = () => {
-  const [isLocked, setIsLocked] = useState();
-  const lockScroll = () => setIsLocked(true);
+  const [isLocked, setIsLocked] = useState(false);
+  const lockScroll = () => {
+    setIsLocked(true);
+  };
   const unLockScroll = () => setIsLocked(false);
 
-  useEffect(() => {}, [isLocked]);
+  useEffect(() => {
+    if (isLocked) {
+      document.getElementsByTagName("body")[0].style.overflow = "hidden";
+    } else {
+      document.getElementsByTagName("body")[0].style.overflow = "visible";
+    }
+  }, [isLocked]);
 
   return [isLocked, { lockScroll, unLockScroll }];
 };
 
 const LockScrollScreen = () => {
+  const [isLocked, { lockScroll, unLockScroll }] = useLockScroll();
   return (
     <div>
       <h2>useLockScroll</h2>
-      <button>Lock Scroll</button>
-      <button>Unlock Scroll</button>
+      <button onClick={lockScroll}>Lock Scroll</button>
+      <button onClick={unLockScroll}>Unlock Scroll</button>
     </div>
   );
 };
